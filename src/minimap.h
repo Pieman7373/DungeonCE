734178ab38918f41248a8cac5d8d386d313de88b
KryptonicDragon <10343367+KryptonicDragon@users.noreply.github.com>
2019-07-18 18:54:45 -0400

    Some comments for minimap.h

--- src/minimap.h
+++ src/minimap.h
@@ -1,6 +1,7 @@
 #ifndef MINIMAP_H
 #define MINIMAP_H
 
+// defines
 #define MINIMAP_COLS 20
 #define MINIMAP_ROWS 14
 #define MINIMAP_TILE_SIZE 5
@@ -9,6 +10,7 @@
 #define MINIMAP_WIDTH (MINIMAP_COLS * MINIMAP_TILE_SIZE)
 #define MINIMAP_HEIGHT (MINIMAP_ROWS * MINIMAP_TILE_SIZE)
 
-void minimap();
+// function prototypes
+void minimap(void);
 
 #endif
